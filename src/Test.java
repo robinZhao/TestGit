d31a9f8d0f12c4aa4e855662fb240a2e02c36cd2
Robin <zrbin153@163.com>
2013-12-13 13:10:48 +0800

    Update Test.java
    添加注释

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,3 @@
-
 public class Test {
 
 	/**
@@ -6,7 +5,7 @@ public class Test {
 	 * 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 打印 testgit
 		System.out.println("testGit");
 
 	}
